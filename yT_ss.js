9865eb0c843b403f4652b19b819d58592c12528a
SAN.D <31897843+SANKARAMDAS@users.noreply.github.com>
2024-11-24 17:26:24 +0530

    Update and rename yt_screensort.js to yT_ss.js
    name changed

--- yT_ss.js
+++ yT_ss.js
@@ -4,13 +4,13 @@ function dataURItoBlob(dataURI) {
 	// doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
 	var byteString = atob(dataURI.split(',')[1]);
   
-	// separate out the mime component
+	//Separate out the mime component
 	var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
   
-	// write the bytes of the string to an ArrayBuffer
+	//Write the bytes of the string to an ArrayBuffer
 	var ab = new ArrayBuffer(byteString.length);
   
-	// create a view into the buffer
+	//Create a view into the buffer
 	var ia = new Uint8Array(ab);
   
 	// set the bytes of the buffer to the correct values
@@ -18,7 +18,7 @@ function dataURItoBlob(dataURI) {
 		ia[i] = byteString.charCodeAt(i);
 	}
   
-	// write the ArrayBuffer to a blob, and you're done
+	//Write the ArrayBuffer to a blob, and you're done
 	var blob = new Blob([ab], {type: mimeString});
 	return blob;
   
